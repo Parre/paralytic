28fd0230c972bc07fcaf7058bcf86ada0160542a
Parre <parre@paralytic.se>
2011-03-19 00:32:18 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paralytic::Application.config.secret_token = '6b49daaefce90857329f0f5c0a4682f847327b83141a701d5860f99ea4e86c7a9db097ca26565787564a74f8991d528c28e6a9befd48fe0a0b4bafaac44c38d6'
